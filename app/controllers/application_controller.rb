a9f25f5b31913a4cceb9ea0811f7bcb49e100aa6
rpavlov <roumen@polymorphic.ca>
2017-02-07 16:49:59 -0500

    Finish auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-
+  before_action :authenticate
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
-  def authorize
+  def authenticate
     redirect_to '/login' unless current_user
   end
   # def authorized_phase?(phase)
